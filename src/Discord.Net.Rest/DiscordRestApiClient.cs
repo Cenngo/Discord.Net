ee8dbac798b79ab3155668325ad3e60886f573ff
Cenngo <cenk.ergen1@gmail.com>
2022-07-22 00:32:48 +0300

    add locale parameter to Get*ApplicationCommandsAsync methods for fetching localized command name/description

--- src/Discord.Net.Rest/DiscordRestApiClient.cs
+++ src/Discord.Net.Rest/DiscordRestApiClient.cs
@@ -1213,9 +1213,17 @@ namespace Discord.API
         #endregion
 
         #region Interactions
-        public async Task<ApplicationCommand[]> GetGlobalApplicationCommandsAsync(bool withLocalizations = false, RequestOptions options = null)
+        public async Task<ApplicationCommand[]> GetGlobalApplicationCommandsAsync(bool withLocalizations = false, string locale = null, RequestOptions options = null)
         {
             options = RequestOptions.CreateOrClone(options);
+
+            if (locale is not null)
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(locale, @"^\w{2}(?:-\w{2})?$"))
+                    throw new ArgumentException($"{locale} is not a valid locale.", nameof(locale));
+
+                options.RequestHeaders["X-Discord-Locale"] = new[] { locale };
+            }
 
             //with_localizations=false doesnt return localized names and descriptions
             return await SendAsync<ApplicationCommand[]>("GET", () => $"applications/{CurrentApplicationId}/commands{(withLocalizations ? "?with_localizations=true" : string.Empty)}",
@@ -1284,9 +1292,17 @@ namespace Discord.API
             return await SendJsonAsync<ApplicationCommand[]>("PUT", () => $"applications/{CurrentApplicationId}/commands", commands, new BucketIds(), options: options).ConfigureAwait(false);
         }
 
-        public async Task<ApplicationCommand[]> GetGuildApplicationCommandsAsync(ulong guildId, bool withLocalizations = false, RequestOptions options = null)
+        public async Task<ApplicationCommand[]> GetGuildApplicationCommandsAsync(ulong guildId, bool withLocalizations = false, string locale = null, RequestOptions options = null)
         {
             options = RequestOptions.CreateOrClone(options);
+
+            if (locale is not null)
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(locale, @"^\w{2}(?:-\w{2})?$"))
+                    throw new ArgumentException($"{locale} is not a valid locale.", nameof(locale));
+
+                options.RequestHeaders["X-Discord-Locale"] = new[] { locale };
+            }
 
             var bucket = new BucketIds(guildId: guildId);
 
